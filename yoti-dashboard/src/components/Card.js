01a9dfe522da263900c365102a817e8fd504feef
Albert Vallverdu <byverdu@alberts-mbp.home>
2018-08-10 09:39:52 +0100

    added attributes items to Card component

--- yoti-dashboard/src/components/Card.js
+++ yoti-dashboard/src/components/Card.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Image from './Image';
+import utils from '../utils/';
 import userLogo from '../assets/images/logged_user.jpeg';
 import defaultLogo from  '../assets/images/default_user.png';
 
@@ -15,11 +16,49 @@ const CardImageShare = ({attributes}) => {
     <Image imgSrc={defaultLogo} />
 }
 
-const CardImage = (cardData) => cardData.type === 'application' ?
+const CardImage = cardData => cardData.type === 'application' ?
   <Image imgSrc={userLogo} /> :
   <CardImageShare {...cardData.transaction} />
 
-const Card = (cardData) => {
+const cardTimeStamp = timeStamp => `at ${utils.getHoursFromUnix(timeStamp)} on ${utils.getDateFromUnix(timeStamp)}`;
+
+const CardTitle = cardData => cardData.type === 'application' ?
+  <Fragment>
+    <h3>Simple Yoti SDK App</h3>
+    <p>
+      viewed this information about you <br />
+      {cardTimeStamp(cardData.transaction['unix-timestamp'])}
+    </p>
+  </Fragment> :
+  <h3>Yoti Shared</h3>;
+
+const CardAttributes = cardData => {
+  const {attributes} = cardData.transaction;
+  const attrMap = {
+    'given-names': 'Given Name(s)',
+    'mobile-number': 'Mobile Number',
+    'gender': 'Gender',
+    'selfie': 'Picture',
+    'email-address': 'Email Address',
+    'age': 'Age',
+    'adress': 'Adress'
+  };
+  const atrrValues = Object.keys(attributes);
+  
+  console.log(attrMap, atrrValues);
+
+  return attributes.map(attr => {
+    const key = Object.keys(attr)[0];
+    return (
+      <Fragment>
+        <h6>{attrMap[key]}</h6>
+        <p>{key === 'selfie' ? 'Yes' : attr[key]}</p>
+      </Fragment>
+    )
+  })
+}
+
+const Card = cardData => {
   return (
     <section>
       {
@@ -27,8 +66,16 @@ const Card = (cardData) => {
       }
       <main>
       {
-        CardImage(cardData)
+        <CardImage {...cardData} />
       }
+      <section>
+        {
+          <CardTitle {...cardData} />        
+        }
+        {
+          <CardAttributes {...cardData} />        
+        }
+      </section>
       </main>
     </section>
   )
